d683ecceb44184899e64384d7699628a803f1183
adeniji4 <128931940+adeniji4@users.noreply.github.com>
2025-06-19 16:12:13 +0100

    Update UpdateProduct.jsx

--- AdminPanel/src/components/UpdateProduct.jsx
+++ AdminPanel/src/components/UpdateProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import { backendUrl} from '../App';
+// import { backendUrl} from '../App';
 import { toast } from 'react-toastify';
 import { assets } from '../assets/assets';
 
